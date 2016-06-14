b56b0805156eb20b72ca245531a2bc498a2f3bf9
Nileesha Fernando <nileeshasajani@hotmail.com>
2016-06-14 08:07:27 -0400

    pulls pictures from getty images api based on weather condition

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+    //City and Weather description are needed to generated images
+  
   //When the submit button is clicked, execute this function
 	$("#submit").click("Submit",function(e){
   
@@ -9,6 +10,8 @@ $(document).ready(function(){
 
     //address reads the data in the search box
     var address= $('#search-box').val();
+
+  
 
     //contents in the address is split and stored in an array
     address=address.split(',');
@@ -25,7 +28,11 @@ $(document).ready(function(){
     		url: url,
         dataType : "jsonp",
   			success : function(response) {
-         // console.log(response);
+          var city;
+          var weather_desc;
+          var state;
+
+         console.log(response);
 
           //variables extracted from JSON response for current conditions
   				var location = response['location']['city']+', '+response['location']['state']+', '+response['location']['zip'];
@@ -34,12 +41,15 @@ $(document).ready(function(){
           var feels_like=response['current_observation']['feelslike_f'];
           var description=response['current_observation']['weather'];
           var gif_url=response['current_observation']['icon_url'];
+          city=response['location']['city'];
+          weather_desc=response['current_observation']['weather'];
+          state =response['location']['state'];
 
           //Variables are set to the html page for current Conditions
           $("#Location").html(location);
           $("#Temperature_F").html(temp_f+" °F");
           $("#Temperature_C").html(temp_c+" °C");
-          $("#Feels_like").html("Feels Like: "+feels_like + " °F");
+          $("#Feels_like").html("Feels Like "+feels_like + " °F");
           $("#img_description").html(description );
           document.getElementById('gif_img').innerHTML="<img src= "+gif_url+">";
 
@@ -50,7 +60,7 @@ $(document).ready(function(){
           //Sunday
           object=object["9"];
           daily_gif = object.icon_url;
-          $("#Sun_data").html("Sunday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Sun_data").html("Sunday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Sun_img').innerHTML="<img src="+daily_gif+">";
           
           //Monday
@@ -58,7 +68,7 @@ $(document).ready(function(){
           object=object["3"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Mon_data").html("Monday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Mon_data").html("Monday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Mon_img').innerHTML="<img src="+daily_gif+">";
 
           //Tuesday
@@ -66,7 +76,7 @@ $(document).ready(function(){
           object=object["4"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Tue_data").html("Tuesday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Tue_data").html("Tuesday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Tue_img').innerHTML="<img src="+daily_gif+">";
 
           //Wednesday
@@ -74,7 +84,7 @@ $(document).ready(function(){
           object=object["5"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Wed_data").html("Wednesday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Wed_data").html("Wednesday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Wed_img').innerHTML="<img src="+daily_gif+">";
 
 
@@ -83,7 +93,7 @@ $(document).ready(function(){
           object=object["6"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Thu_data").html("Thursday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Thu_data").html("Thursday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Thu_img').innerHTML="<img src="+daily_gif+">";
 
 
@@ -92,7 +102,7 @@ $(document).ready(function(){
           object=object["7"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Fri_data").html("Friday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Fri_data").html("Friday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Fri_img').innerHTML="<img src="+daily_gif+">";
 
 
@@ -101,13 +111,41 @@ $(document).ready(function(){
           object=object["8"];
           //console.log(object);
           daily_gif = object.icon_url;
-          $("#Sat_data").html("Saturday:"+"<br>" +"High: "+object.high.fahrenheit+ "<br>"+"Low: "+object.low.fahrenheit);
+          $("#Sat_data").html("Saturday"+"<br>" +"High: "+object.high.fahrenheit+" °F"+ "<br>"+"Low: "+object.low.fahrenheit+" °F");
           document.getElementById('Sat_img').innerHTML="<img src="+daily_gif+">";
+          console.log('city'+city);
+          console.log('weather'+weather_desc);
 
-
+          gettyImages(state,weather_desc);
  			 }
 		});
+    
+    function gettyImages(state,weather_desc){
+    var apiKey = '9upx676y8p8ufzwbjsu6q5fd';
+    var keyWord =weather_desc+" sky";
+    console.log('weather_desc = '+keyWord);
+    $.ajax({
+        type:'GET',
+        url:"https://api.gettyimages.com:443/v3/search/images?phrase="+keyWord,
+         beforeSend: function (request)
+            {
+                request.setRequestHeader("Api-Key", apiKey);
+            }})
+        .done(function(data){
+        console.log("Success with data")
+        console.log(data);
+        var imgURL =data.images[0];
+        imgURL=imgURL.display_sizes[0];
+        imgURL=imgURL.uri;
+        console.log('url'+imgURL);
+        document.getElementById('tester').innerHTML="<img src="+imgURL+">";
+        document.getElementById('current_result').backgroundImage = "url('img1.jpeg')";
 
+      })
+     .fail(function(data){
+          alert(JSON.stringify(data,2))
+     });
+   }
 
     });
   
